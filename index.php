1ee415f9c432bc12551d649ae3d8a603dd296018
Max <maxblanck@outlook.fr>
2020-12-08 16:06:10 +0100

    php Partie 3  Exercice 8

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercice 8 partie 3</title>
+</head>
+
+<body>
+    <h1>Exercice 8 Partie 3</h1>
+
+    <ul>
+        <?php
+        for ($number = 200; $number >= 0; $number -= 12) {
+            echo '<li>' . 'Enfin !!!!' . '</li>';
+        }
+        ?></ul>
+</body>
+
+</html>
